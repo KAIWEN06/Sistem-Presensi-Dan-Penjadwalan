538d2c105bb995a6e6591f82bb289c4e462ee070
KAIWEN06 <kevinsondakh2006@gmail.com>
2026-06-03 18:13:03 +0800

    1

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 console.log(import.meta.env);
 console.log("URL =", import.meta.env.VITE_SUPABASE_URL);
 console.log("KEY =", import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log("FULL ENV", import.meta.env);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
